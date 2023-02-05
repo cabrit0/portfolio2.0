12a6daf254775f258e98da40590f9ad2b8a32b6b
cabrit0 <cabrit0o.dev@gmail.com>
2023-02-05 16:24:53 +0000

    projectsCard layout fix

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -8,22 +8,30 @@ const ProjectsCard = (project) => {
       className="h-full w-full flex flex-col items-center justify-between bg-myGreenDark"
       whileHover={{ scale: 1.05, boxShadow: "0px 0px 15px #888888" }}
     >
-      <div className="h-96 overflow-hidden px-16 md:px-20 py-2 rounded-2xl">
-        <img src={project.img} alt={project.name} className="w-full h-full " />
-      </div>
-      <div className="px-6 py-4">
-        <h2 className="text-2xl font-medium text-myGreen">{project.name}</h2>
-        <p className="text-myBege text-md mt-2">{project.description}</p>
-        <div className="flex flex-wrap mt-4">
-          {project.technologies.map((tech) => (
-            <span
-              key={tech}
-              className="px-4 py-1 text-sm text-white bg-myGreen rounded-full mr-2 mb-2"
-            >
-              {tech}
-            </span>
-          ))}
+      <div>
+        <div className="h-96 overflow-hidden px-16 md:px-20 py-2 rounded-2xl">
+          <img
+            src={project.img}
+            alt={project.name}
+            className="w-full h-full "
+          />
         </div>
+        <div className="px-6 py-4">
+          <h2 className="text-2xl font-medium text-myGreen">{project.name}</h2>
+          <p className="text-myBege text-md mt-2">{project.description}</p>
+          <div className="flex flex-wrap mt-4">
+            {project.technologies.map((tech) => (
+              <span
+                key={tech}
+                className="px-4 py-1 text-sm text-white bg-myGreen rounded-full mr-2 mb-2"
+              >
+                {tech}
+              </span>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="mb-4">
         <div className="mt-4 flex items-center justify-center">
           <motion.button
             className="text-myGreen text-sm font-medium px-4 py-2 rounded-lg duration-300 hover:text-myYellow"
